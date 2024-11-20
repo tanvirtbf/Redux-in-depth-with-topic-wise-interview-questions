29bce3889eab12e03b165469926dbe4d73339984
tanvirtbf <tanver018765@gmail.com>
2024-11-21 01:36:26 +0600

    my own redux created

---
+++ 3. Own Redux/script.js
@@ -0,0 +1,38 @@
+import { myCreateStore } from "./my-redux"
+
+const initialState = {
+  post: 0,
+  name : 'Tanvir Ahmed',
+  age: 25,
+}
+
+const INCREMENT = 'post/increment'
+const DECREMENT = 'post/decrement'
+const INCREASEBY = 'post/increaseBy'
+const DECREASEBY = 'post/decreaseBy'
+
+function reducer(state=initialState, action) {
+  switch(action.type){
+    case INCREMENT : 
+      return {...state, post : state.post + 1}
+    case DECREMENT : 
+      return {...state, post : state.post - 1}
+    case INCREASEBY : 
+      return {...state, post : state.post + action.payload}
+    case DECREASEBY : 
+      return {...state, post : state.post - action.payload}
+    default : 
+      return state 
+  }
+}
+
+const store = myCreateStore(reducer)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch({type : INCREMENT})
+store.dispatch({type : INCREMENT})
+store.dispatch({type : DECREASEBY, payload : 3})
+store.dispatch({type : INCREASEBY, payload : 5})
