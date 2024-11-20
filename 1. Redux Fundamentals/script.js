915a595d33eed25d382fcc239ff8e0eb494987e9
tanvirtbf <tanver018765@gmail.com>
2024-11-20 14:55:46 +0600

    example of payload

--- 1. Redux Fundamentals/script.js
+++ 1. Redux Fundamentals/script.js
@@ -9,7 +9,12 @@ function reducer(state, action) {
     return {...state, post : state.post + 1}
   }else if(action.type === 'post/decrement'){
     return {...state, post : state.post - 1}
+  }else if(action.type === 'post/incrementBy'){
+    return {...state, post : state.post + action.payload}
+  }else if(action.type === 'post/decrementBy'){
+    return {...state, post : state.post - action.payload}
   }
+  return state
 }
 
 reduxState = reducer(reduxState, {type : 'post/increment'})
@@ -26,3 +31,11 @@ reduxState = reducer(reduxState, {type : 'post/increment'})
 console.log(reduxState) // {post: 4, name: 'Tanvir Ahmed', age: 25}
 reduxState = reducer(reduxState, {type : 'post/decrement'})
 console.log(reduxState) // {post: 3, name: 'Tanvir Ahmed', age: 25}
+
+
+reduxState = reducer(reduxState, {type : 'post/incrementBy', payload: 1})
+console.log(reduxState) // {post: 4, name: 'Tanvir Ahmed', age: 25}
+reduxState = reducer(reduxState, {type : 'post/incrementBy', payload: 4})
+console.log(reduxState) // {post: 8, name: 'Tanvir Ahmed', age: 25}
+reduxState = reducer(reduxState, {type : 'post/decrementBy', payload: 3})
+console.log(reduxState) // {post: 5, name: 'Tanvir Ahmed', age: 25}
