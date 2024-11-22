0f8d3623a79ada4d48fa5f57abf4a8846553c3ea
tanvirtbf <tanver018765@gmail.com>
2024-11-22 11:56:07 +0600

    card item dispatch and reducer complete

--- 4. Manage Complex State using Redux/script.js
+++ 4. Manage Complex State using Redux/script.js
@@ -1,22 +1,67 @@
 import { createStore } from "redux";
-
-function actionObj(type, payload={}){
-  return { type : type, payload : payload}
-}
+import { productsList } from "./productsList";
 
 const initialState = {
-  post : 0, 
-  name : 'Tanvir Ahmed',
-  age : 25,
+  productsList : productsList,
+  cardItems : [],
+  wishList : [],
 }
 
-function reducer(state=initialState, action){
+const ADD_CARD_ITEM = 'card/addCardItems'
+const REMOVE_CARD_ITEM = 'card/removeCardItems'
+const INCREASE_CARD_ITEM = 'card/increaseCardItem'
+const DECREASE_CARD_ITEM = 'card/decreaseCardItem'
+
+const ADD_WISHLIST_ITEM = 'wishlist/addItem'
+const REMOVE_WISHLIST_ITEM = 'wishlist/removeItem'
+
+function reducer(state=initialState, action) {
+
+  let newState = state;
+
+  switch(action.type) {
+    case ADD_CARD_ITEM : 
+      newState = {...newState, cardItems : [...newState.cardItems , action.payload]}
+      break;
+    case REMOVE_CARD_ITEM : 
+      newState = {...newState, cardItems : newState.cardItems.filter((item) => item.productId !== action.payload.productId)}
+      break;
+    case INCREASE_CARD_ITEM : 
+      newState = {...newState, cardItems : newState.cardItems.map((item) => {
+        if(item.productId === action.payload.productId){
+          return {...item, quantity : item.quantity + 1}
+        }
+      })}
+      break;
+    case DECREASE_CARD_ITEM : 
+      newState = {...newState, cardItems : newState.cardItems.map((item) => {
+        if(item.productId === action.payload.productId) {
+          return {...item, quantity : item.quantity - 1}
+        }
+      })}
+      break;
+  }
 
   return newState
 }
 
-const {dispatch, getState, subscribe} = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.())
 
-subscribe(()=> {
-  console.log(getState())
+store.dispatch({type : ADD_CARD_ITEM, payload : { productId : 3, quantity : 2 }})
+store.dispatch({type : ADD_CARD_ITEM, payload : { productId : 5, quantity : 3 }})
+store.dispatch({type : ADD_CARD_ITEM, payload : { productId : 2, quantity : 2 }})
+
+store.dispatch({type : REMOVE_CARD_ITEM, payload: { productId : 3 }})
+store.dispatch({type : REMOVE_CARD_ITEM, payload: { productId : 2 }})
+
+store.dispatch({type : INCREASE_CARD_ITEM, payload : {productId : 5}})
+store.dispatch({type : INCREASE_CARD_ITEM, payload : {productId : 5}})
+store.dispatch({type : INCREASE_CARD_ITEM, payload : {productId : 5}})
+
+store.dispatch({type : DECREASE_CARD_ITEM, payload : {productId : 5}})
+store.dispatch({type : DECREASE_CARD_ITEM, payload : {productId : 5}})
+
+store.subscribe(()=>{
+  console.log(store.getState())
 })
+
