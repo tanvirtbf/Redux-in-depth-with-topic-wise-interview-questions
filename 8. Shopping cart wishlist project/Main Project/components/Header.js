a3cd25f87fef1d9d6e331d4153d760a12e595728
tanvirtbf <tanver018765@gmail.com>
2024-11-25 23:33:01 +0600

    update

--- 8. Shopping cart wishlist project/Main Project/components/Header.js
+++ 8. Shopping cart wishlist project/Main Project/components/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "../assets/cart-icon.svg";
-import WishListIcon from '../assets/heart-svgrepo-com.svg'
+import WishListIcon from "../assets/heart-svgrepo-com.svg";
 import { useSelector } from "react-redux";
 
 export default function Header() {
-  
   const cartItems = useSelector((state) => state.cartItems);
-  const wishlists = useSelector(state => state.wishList)
-  console.log(wishlists)
+  const wishlists = useSelector((state) => state.wishList);
+  console.log(wishlists);
   return (
     <header>
       <div className="header-contents">
@@ -17,9 +16,7 @@ export default function Header() {
         </h1>
         <Link className="cart-icon" to="/wishlist">
           <img src={WishListIcon} alt="cart-icon" />
-          <div className="cart-items-count">
-            {wishlists.length}
-          </div>
+          <div className="cart-items-count">{wishlists.length}</div>
         </Link>
         <Link className="cart-icon" to="/cart">
           <img src={CartIcon} alt="wishlist-icon" />
