3845483afc66f75de04b540bcd84b250c758d2e5
tanvirtbf <tanver018765@gmail.com>
2024-11-26 15:41:20 +0600

    product fetch loading effect and error showing part compelte

--- 10. Making Api Call/pages/Home.js
+++ 10. Making Api Call/pages/Home.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import Product from '../components/Product'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
+import Product from "../components/Product";
 
 export default function Home() {
-  const productsList = useSelector((state) => state.products)
+  const productsList = useSelector((state) => state.products.list);
+  const isLoading = useSelector((state) => state.products.loading);
+  const isError = useSelector((state) => state.products.error);
 
   return (
-    <div className="products-container">
-      {productsList.map(({ id, title, rating, price, image }) => (
-        <Product
-          key={id}
-          productId={id}
-          title={title}
-          rating={rating.rate}
-          price={price}
-          imageUrl={image}
-        />
-      ))}
-    </div>
-  )
+    <>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        isError || (
+          <div className="products-container">
+            {productsList.map(({ id, title, rating, price, image }) => (
+              <Product
+                key={id}
+                productId={id}
+                title={title}
+                rating={rating.rate}
+                price={price}
+                imageUrl={image}
+              />
+            ))}
+          </div>
+        )
+      )}
+    </>
+  );
 }
